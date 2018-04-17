12fb47de0543222c931b1dd75b50349a152a1a1c
Wilkerson <brywilk@m-c02sxbxggtf1.local>
2018-04-17 14:52:07 -0400

    5

--- setup_terminal_customization.sh
+++
@@ -1,9 +0,0 @@
-
-# Pull down oh my zsh
-git clone git@github.com:brywilk/oh-my-zsh.git
-
-# Set shell
-chsh -s /bin/zsh
-
-# Install dot files
-curl -Lks https://raw.githubusercontent.com/brywilk/dotfiles/master/dotfile-install.sh | /bin/bash
