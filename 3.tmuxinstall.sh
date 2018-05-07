52a1a9370e22aeda52797a816917c9bdbfb2c097
Bryan A Wilkerson <brywilk@its-appint01.dsc.umich.edu>
2018-05-07 16:35:58 -0400

    tmuxinstall

---
+++ 3.tmuxinstall.sh
@@ -0,0 +1,6 @@
+yum install ruby
+# gem list tmuxinator
+# gem list tmuxinator --remote
+# gem list tmuxinator --all
+# The one that works with the supported version of ruby is 0.9.0
+gem install tmuxinator -v0.9.0
