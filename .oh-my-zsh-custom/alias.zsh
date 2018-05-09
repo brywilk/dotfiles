316f156269cffc8636256a863f5cc41070b55018
brywilk <brywilk@gmail.com>
2018-05-09 17:36:51 -0400

    alias

--- .oh-my-zsh-custom/alias.zsh
+++ .oh-my-zsh-custom/alias.zsh
@@ -1,2 +1,67 @@
 alias gitupdate='~/"Personal and Private"/OneDrive/Scripts/git_update_all.sh'
 alias tcheat='less /Users/brywilk/Personal\ and\ Private/OneDrive/Tmux/tmux.cheat'
+
+# Nagios
+='ssh diablotin.dsc.umich.edu'
+='ssh moa.dsc.umich.edu'
+='ssh magdalena.dsc.umich.edu'
+='ssh shoebill.dsc.umich.edu'
+='ssh cardinal.dsc.umich.edu'
+
+# Radmin
+='ssh bufflehead.dsc.umich.edu'
+
+# Bastions
+alias alethe='ssh alethe.dsc.umich.edu'
+alias grassbird='ssh grassbird.dsc.umich.edu'
+alias mandarin='ssh mandarin.dsc.umich.edu'
+alias baza='ssh p-brywilk@p-baza.enclave.it.umich.edu'
+alias awsjump='ssh brywilk@jump-box.aws.vdc.it.umich.edu'
+alias atlantis='ssh atlantis.gpcc.umich.edu'
+alias appint='ssh brywilk@its-appint01.dsc.umich.edu'
+
+# Tripwire
+alias trip='ssh p-brywilk@p-tripwire.enclave.it.umich.edu'
+
+# Splunk
+alias spin='ssh spinifexbird.dsc.umich.edu'
+alias storm='ssh stormpetrel.dsc.umich.edu'
+alias hec1='ssh splk-hf-listener1.dsc.umich.edu'
+alias hec2='ssh splk-hf-listener2.dsc.umich.edu'
+alias dep='ssh splk-dep.dsc.umich.edu'
+alias shc1='ssh splk-shc1.dsc.umich.edu'
+alias shc2='ssh splk-shc2.dsc.umich.edu'
+alias shc3='ssh splk-shc3.dsc.umich.edu'
+alias shs='ssh splk-shs.dsc.umich.edu'
+alias icm='ssh splk-icm.dsc.umich.edu'
+alias idx1='ssh splk-idx1.dsc.umich.edu'
+alias idx2='ssh splk-idx2.dsc.umich.edu'
+alias idx3='ssh splk-idx3.dsc.umich.edu'
+alias idx4='ssh splk-idx4.dsc.umich.edu'
+alias idx5='ssh splk-idx5.dsc.umich.edu'
+alias idx6='ssh splk-idx6.dsc.umich.edu'
+alias idx7='ssh splk-idx7.dsc.umich.edu'
+alias idx8='ssh splk-idx8.dsc.umich.edu'
+alias idx9='ssh splk-idx9.dsc.umich.edu'
+alias idx10='ssh splk-idx10.dsc.umich.edu'
+alias hf3='ssh brywilk@splk-hf01.aws.vdc.it.umich.edu'
+alias hf2='ssh brywilk@splk-hf02.aws.vdc.it.umich.edu'
+alias deplcient='ssh brywilk@splk-depclient.aws.vdc.it.umich.edu'
+
+# ITAM
+alias itam1='ssh itam-license1.dsc.umich.edu'
+alias itam2='ssh itam-license2.dsc.umich.edu'
+alias itam3='ssh itam-license3.dsc.umich.edu'
+alias lsa1='ssh license1.lsa.umich.edu'
+alias lsa2='ssh license2.lsa.umich.edu'
+alias lsa3='ssh license3.lsa.umich.edu'
+
+# Pinnacle
+alias liwi='ssh liwi.dsc.umich.edu'
+alias trumpet='ssh trumpet.dsc.umich.edu'
+
+# Crashplan
+alias crashplan='='ssh partridge.dsc.umich.edu'
+
+# Sysloggers
+alias legion='='ssh legion.dsc.umich.edu'
